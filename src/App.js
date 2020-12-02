2377ae7764a5f50c0f3d81306293bcaae157e018
devWemillyCristhiny <devwemilly.frontend@gmail.com>
2020-12-02 16:20:01 -0300

    Terminado

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './App.css';
+import ButtonAnimation from './components/ButtonAnimation'
+
+function App() {
+  return (
+    <ButtonAnimation/>
+  );
+}
+
+export default App;
---
+++ src/components/ButtonAnimation.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './ButtonAnimation.css'
+
+export default function(){
+    return (
+        <div className="container">
+            <a href="#">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                Neon Button
+            </a>
+        </div>
+    )
+}
